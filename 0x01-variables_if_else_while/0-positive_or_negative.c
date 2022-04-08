702cadd1b1221c577efe695fee2009bc73a942fd
Deborah <semiloremuriana@gmail.com>
2022-04-08 09:31:31 -0700

    still task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - Entry point
  *
- * Description - to determine whether a random number is 
+ * Description - to determine whether a random number is
  * positive, negative or zero
  * Return: Always 0 (Success)
  */
@@ -16,7 +16,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n < 0)
 		printf("%d is negative\n", n);
 	else if (n == 0)
