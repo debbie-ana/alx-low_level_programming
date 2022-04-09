d3a50881d9b73f98d6428ac0117cf03372ca92d7
Deborah <semiloremuriana@gmail.com>
2022-04-09 06:09:31 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,19 +4,20 @@
  * main - Entry point
  *
  * Description - printing numbers 0 to 9
+ * using only putchar
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int num = 0;
+	int num = 48;
 	/* using variable num to represent number*/
 
-	while (num < 10)
+	while (num < 58)
 	{
-		printf(num);
+		putchar(num);
 		num = num + 1;
 	}
-	printf('\n');
+	putchar('\n');
 	return (0);
 }
