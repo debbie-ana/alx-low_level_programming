8ea6f4fa214424152a7d053760722453995a6289
Deborah <semiloremuriana@gmail.com>
2022-04-08 17:44:33 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - printing numbers 0 to 9
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = 0;
+	/* using variable num to represent number*/
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num = num + 1;
+	}
+	printf("\n");
+	return (0);
+}
