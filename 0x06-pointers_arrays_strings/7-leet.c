0964741181ec78fc0c39835802027758c9f3c782
Deborah <semiloremuriana@gmail.com>
2022-04-23 15:41:34 -0700

    task 6 again and 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+/**
+ * leet -encodes a string into 1337
+ * @s: string to be encoded
+ *
+ * Return: encoded string
+ */
+
+char *leet(char *s)
+{
+	char *r = s;
+	char a[] = {'a', 'e', 'o', 't', 'l'};
+	char n[] = {4, 3, 0, 7, 1};
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*s == a[i] || *s == a[i] - 32)
+				*s = n[i] + '0';
+		}
+		s++;
+	}
+	return (r);
+}
