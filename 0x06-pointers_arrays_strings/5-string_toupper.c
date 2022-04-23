aa736ac74bdbf8656f4fecd319a941639b42813c
Deborah <semiloremuriana@gmail.com>
2022-04-23 14:25:48 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+/**
+ * string_toupper - changes all lowercase letters of string to uppercase
+ * @s: pointer to string
+ *
+ * Return: pointer to string
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 32;
+	}
+	return (s);
+}
