ac5cdbb74f03b1a5885dc813f661c54865a40777
Deborah <semiloremuriana@gmail.com>
2022-04-28 09:01:09 -0700

    task 5 again

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,34 @@
 /**
+ * isqrt - checks for the square root of a number
+ * @start: number to start with to check
+ * @end:  number to end with to check
+ * @n: number whose square root is to be gotten
+ *
+ * Return: square root if square root is natural number, -1 otherwise
+ */
+int isqrt(int start, int end, int n)
+{
+	int mid;
+
+	if (end >= start)
+	{
+		mid = start + (end - start) / 2;
+		if (mid * mid == n)
+			return (mid);
+		if (mid * mid > n)
+			return (isqrt(start, mid - 1, n));
+		if (mid * mid < n)
+			return (isqrt(mid + 1, end, n));
+	}
+	return (-1);
+}
+
+/**
  * _sqrt_recursion - returns natural square root of a number
  * @n: number to get square root of
  *
  * Return: square root
  */
-int isqrt(int k, int n);
 
 int _sqrt_recursion(int n)
 {
@@ -12,29 +36,5 @@ int _sqrt_recursion(int n)
 		return (-1);
 	else if (n < 2)
 		return (n);
-	return (isqrt(1, n));
-}
-
-/**
- * isqrt - checks for the square root of a number
- * @k: number to start with to check
- * @n: number whose square root is to be gotten
- *
- * Return: square root if square root is natural number, -1 otherwise
- */
-
-int isqrt(int k, int n)
-{
-	int next = (k + n / k) / 2;
-
-	if (k * k <= n && (k + 1) * (k + 1) > n)
-	{
-		if ((k * k) == n)
-			return (k);
-		return (-1);
-	}
-	else
-	{
-		return (isqrt(next, n));
-	}
+	return (isqrt(2, n, n));
 }
