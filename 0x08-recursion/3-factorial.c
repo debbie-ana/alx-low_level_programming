ec99fd7dbcadd76f745cf35a60b60883d7e3f306
Deborah <semiloremuriana@gmail.com>
2022-04-28 06:21:50 -0700

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+/**
+ * factorial - returns the factorial of a given number
+ * @n: number
+ *
+ * Return: value of factorial
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
