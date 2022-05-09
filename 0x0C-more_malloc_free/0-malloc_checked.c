5fca9593c5852804385b1f0d236e55ac546d19a2
Deborah <semiloremuriana@gmail.com>
2022-05-09 08:29:55 -0700

    task 0, more malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes
+ *
+ * Return: pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	if (malloc(b) == NULL)
+		exit (98);
+	else
+		return (malloc(b));
+}
