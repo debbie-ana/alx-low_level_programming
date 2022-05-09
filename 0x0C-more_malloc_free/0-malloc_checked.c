8bec7e716c6511693e9660097c71b7ebdef7f1ae
Deborah <semiloremuriana@gmail.com>
2022-05-09 09:12:26 -0700

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <limits.h>
 /**
  * malloc_checked - allocates memory using malloc
  * @b: number of bytes
@@ -10,7 +10,7 @@
 
 void *malloc_checked(unsigned int b)
 {
-	if (malloc(b) == NULL)
+	if (malloc(b) == NULL || b > INT_MAX - 1)
 		exit(98);
 	else
 		return (malloc(b * sizeof(char)));
