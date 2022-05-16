dcaec5efa6d1b66082345b247fa3a3fbf9df0542
Deborah <semiloremuriana@gmail.com>
2022-05-16 08:53:35 -0700

    1 again

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,3 +1,4 @@
+#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
 
@@ -15,8 +16,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
-	if (n == 0)
-		return;
 	va_start(ap, n);
 	for (i = 0; i < n - 1; i++)
 	{
