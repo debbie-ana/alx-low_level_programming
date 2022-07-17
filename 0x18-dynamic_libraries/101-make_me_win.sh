ae398bb33e998574292fe3ef66008b716c179728
Deborah <semiloremuriana@gmail.com>
2022-07-16 17:27:43 -0700

    trying

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/debbie-ana/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
-export LD_PRELOAD=/tmp/libmask.so
+wget -P /tmp https://github.com/debbie-ana/alx-low_level_programming/raw/master/0x18-dynamic_libraries/101-lib.so
+export LD_PRELOAD=/tmp/101-lib.so
