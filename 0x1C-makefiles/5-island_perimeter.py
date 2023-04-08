97a0934f31aa1fe931863c4c396ae3bd28a0355b
debbie-ana <semiloremuriana@gmail.com>
2023-04-08 11:39:48 -0700

    4

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+"""function for perimeter of island described in grid"""
+
+def island_perimeter(grid):
+    """
+    returns the perimeter of the island described in grid
+    """
+    width = len(grid[0])
+    height = len(grid)
+
+    for row in grid:
+        for cell in row:
+            if
