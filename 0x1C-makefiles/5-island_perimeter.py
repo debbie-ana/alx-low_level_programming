df995731dfca3f85ec78584b46e575fc126dd03d
debbie-ana <semiloremuriana@gmail.com>
2023-04-09 02:24:12 -0700

    5...

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,13 +9,13 @@ def island_perimeter(grid):
     breadth = len(grid[0])
     length = len(grid)
     i_length = 0
-    i_breadth = 0
+    # i_breadth = 0
 
     for lt in range(length):
         for b in range(breadth):
             if (grid[lt][b] == 1):
                 i_length = i_length + 1
-            #if (grid[lt][b-1] == 1 or grid[lt][b+1] == 1):
-              #      i_breadth = i_breadth + 1
+            # if (grid[lt][b-1] == 1 or grid[lt][b+1] == 1):
+            # i_breadth = i_breadth + 1
 
     return (i_length + 1) * 2
